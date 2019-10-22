4f14c8d09b07c694b1f247d4c66c0002a96717aa
Alan Solis Flores <alansolisflores@outlook.com>
2019-10-22 15:28:58 -0500

    Simple observable example

--- app/src/main/java/com/alansolisflores/rxjava/MainActivity.kt
+++ app/src/main/java/com/alansolisflores/rxjava/MainActivity.kt
@@ -2,11 +2,16 @@ package com.alansolisflores.rxjava
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
 
 class MainActivity : AppCompatActivity(), View.OnClickListener{
+
+    private val TAG = "MainActivity"
 
     private lateinit var taskButton: Button
 
@@ -18,9 +23,42 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
 
     private fun initProperties(){
         this.taskButton = findViewById(R.id.taskButton)
+        this.taskButton.setOnClickListener(this)
     }
 
     override fun onClick(p0: View?) {
-        //Intentionally blank
+        startRStream()
+    }
+
+    private fun startRStream(){
+        val myObservable = getObservable()
+
+        val myObserver = getObserver()
+
+        myObservable.subscribe(myObserver)
+    }
+
+    private fun getObserver(): Observer<String>{
+        return object: Observer<String>{
+            override fun onSubscribe(d: Disposable) {
+
+            }
+
+            override fun onError(e: Throwable) {
+                Log.d(TAG,"onError: "+e.message)
+            }
+
+            override fun onComplete() {
+                Log.d(TAG,"onComplete")
+            }
+
+            override fun onNext(t: String) {
+                Log.d(TAG,"onNext $t")
+            }
+        }
+    }
+
+    private fun getObservable(): Observable<String> {
+        return Observable.just("1","2","3","4","5")
     }
 }
